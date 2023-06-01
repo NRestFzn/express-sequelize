29647c1124651cc86040decb1c362866d34aaaa3
Lenovo <nashirresta7@gmail.com>
2023-06-01 08:16:54 +0700

    update/add admin docs

--- docs/swagger/routes/user.js
+++ docs/swagger/routes/user.js
@@ -45,95 +45,12 @@ module.exports = {
       },
     },
   },
-  '/user': {
-    get: {
-      tags: ['User'],
-      summary: 'Get all user',
-      security: [],
-      produces: ['application/json'],
-      responses: {
-        200: {
-          description: 'OK',
-        },
-      },
-    },
-  },
-
-  '/user/{id}': {
-    get: {
-      tags: ['User'],
-      summary: 'Get user by id',
-      security: [],
-      parameters: [
-        {
-          in: 'path',
-          name: 'id',
-          schema: {
-            type: 'integer',
-          },
-          example: 1,
-        },
-      ],
-      produces: ['application/json'],
-      responses: {
-        200: {
-          description: 'OK',
-        },
-      },
-    },
-  },
 
   'user/me': {
     get: {
       tags: ['User'],
       summary: 'Get user by token',
       security: [{ tokenpublic: [] }],
-      produces: ['application/json'],
-      responses: {
-        200: {
-          description: 'OK',
-        },
-      },
-    },
-  },
-
-  'user/update/{id}': {
-    put: {
-      tags: ['User'],
-      summary: 'update user by id',
-      security: [],
-      parameters: [
-        {
-          in: 'path',
-          name: 'id',
-          schema: {
-            type: 'integer',
-          },
-          example: 1,
-        },
-      ],
-      requestBody: {
-        required: true,
-        content: {
-          'application/x-www-form-urlencoded': {
-            schema: {
-              type: 'object',
-              properties: {
-                username: {
-                  type: 'string',
-                },
-                fullname: {
-                  type: 'string',
-                },
-                email: {
-                  type: 'string',
-                },
-              },
-              required: ['username', 'email', 'fullname'],
-            },
-          },
-        },
-      },
       produces: ['application/json'],
       responses: {
         200: {
@@ -202,78 +119,6 @@ module.exports = {
       },
       responses: {
         201: {
-          description: 'OK',
-        },
-      },
-    },
-  },
-
-  '/user/softdelete/{id}': {
-    delete: {
-      tags: ['User'],
-      summary: 'soft delete user by id',
-      security: [],
-      parameters: [
-        {
-          in: 'path',
-          name: 'id',
-          schema: {
-            type: 'integer',
-          },
-          example: 1,
-        },
-      ],
-      produces: ['application/json'],
-      responses: {
-        200: {
-          description: 'OK',
-        },
-      },
-    },
-  },
-
-  '/user/forcedelete/{id}': {
-    delete: {
-      tags: ['User'],
-      summary: 'force delete user by id',
-      security: [],
-      parameters: [
-        {
-          in: 'path',
-          name: 'id',
-          schema: {
-            type: 'integer',
-          },
-          example: 1,
-        },
-      ],
-      produces: ['application/json'],
-      responses: {
-        200: {
-          description: 'OK',
-        },
-      },
-    },
-  },
-
-  '/user/restore/{id}': {
-    put: {
-      tags: ['User'],
-      summary: 'restore deleted user by id',
-      security: [],
-      parameters: [
-        {
-          in: 'path',
-          name: 'id',
-          schema: {
-            type: 'integer',
-          },
-          example: 1,
-        },
-      ],
-      produces: ['application/json'],
-      responses: {
-        200: {
           description: 'OK',
         },
       },
