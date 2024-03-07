033ee6e42c974b1ec9c7c516fefbc2813ae2d0cf
Lenovo <nashirresta7@gmail.com>
2024-03-07 21:49:31 +0700

    fix: change message on http response

--- src/modules/response/HttpResponse.js
+++ src/modules/response/HttpResponse.js
@@ -10,25 +10,25 @@ class HttpResponse {
   }
 
   static get(dataResponse, options) {
-    const message = 'success.data_received'
+    const message = 'data received'
 
     return this.baseResponse({ message, ...dataResponse })
   }
 
   static created(dataResponse, options) {
-    const message = 'success.data_added'
+    const message = 'data successfully added'
 
     return this.baseResponse({ code: 201, message, ...dataResponse })
   }
 
   static updated(dataResponse, options) {
-    const message = 'success.data_updated'
+    const message = 'data successfully updated'
 
     return this.baseResponse({ message, ...dataResponse })
   }
 
   static deleted(dataResponse, options) {
-    const message = 'success.data_deleted'
+    const message = 'data successfully deleted'
 
     return this.baseResponse({ message, ...dataResponse })
   }
