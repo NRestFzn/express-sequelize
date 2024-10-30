a1bf3dd3dd52d624251569e3dcdbe2c76c701af3
Lenovo <nashirresta7@gmail.com>
2024-10-30 09:56:03 +0700

    fix: env database

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,11 +1,11 @@
-import env from '@config/env.config'
+require('dotenv').config()
 
 module.exports = {
-  username: env.SEQUELIZE_USERNAME,
-  password: env.SEQUELIZE_PASSWORD,
-  database: env.SEQUELIZE_DATABASE,
-  host: env.SEQUELIZE_HOST,
-  port: env.SEQUELIZE_PORT,
-  dialect: env.SEQUELIZE_CONNECTION,
-  timezone: env.SEQUELIZE_TIMEZONE,
+  username: process.env.SEQUELIZE_USERNAME,
+  password: process.env.SEQUELIZE_PASSWORD,
+  database: process.env.SEQUELIZE_DATABASE,
+  host: process.env.SEQUELIZE_HOST,
+  port: process.env.SEQUELIZE_PORT,
+  dialect: process.env.SEQUELIZE_CONNECTION,
+  timezone: process.env.SEQUELIZE_TIMEZONE,
 }
